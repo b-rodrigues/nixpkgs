e93c594022e652d077419d45d1ab6ce4f5f5c579
Bruno Rodrigues <bruno@brodrigues.co>
2024-08-29 16:39:27 +0200

    progress, but build still failing

--- pkgs/by-name/po/positron/package.nix
+++ pkgs/by-name/po/positron/package.nix
@@ -2,17 +2,20 @@
   stdenv,
   yarn,
   nodejs,
-  jq,
+#  jq,
   git,
-  gcc,
+#  gcc,
   pkg-config,
   libX11,
   libxkbfile,
   libsecret,
   libkrb5,
-  cmake,
+  #cmake,
   lib,
-  fetchFromGitHub,
+  fetchgit,
+  fetchYarnDeps,
+  fixup-yarn-lock,
+  python3,
   quarto
 }:
 
@@ -24,34 +27,93 @@ let
   POSITRON_VERSION_PATCH = lib.versions.patch version;
   POSITRON_VERSION_SUFFIX = "-" + toString (lib.tail (lib.splitString "-" version));
 
-  src = fetchFromGitHub {
-    owner = "posit-dev";
-    repo = "positron";
+  src = fetchgit {
+    url = "https://github.com/posit-dev/positron";
     rev = version;
+    fetchSubmodules = true;
     hash = "sha256-nDbS14F1x/f+jxkQ+bKB2qQi/JXks6gK9zj7Coxchq0=";
+  };
+
+  OfflineCache = fetchYarnDeps {
+    yarnLock = "${src}/yarn.lock";
+    hash = "sha256-++m6+BhWb3HW0Jkcx9wh9uilPM4flLO/KD+9wRzwvqE=";
+  };
+
+  buildOfflineCache = fetchYarnDeps {
+    yarnLock = "${src}/build/yarn.lock";
+    hash = "sha256-sZ052z+z0g3lXPmij2iBPKVAS7rl4RrUVXOy9nqcM2Q=";
+  };
+
+  remoteOfflineCache = fetchYarnDeps {
+    yarnLock = "${src}/remote/yarn.lock";
+    hash = "sha256-kNTdE88ten3zXM64MAh0CJGVXqOh//Z9JUoBSrp6fqY=";
+  };
+  extensionsOfflineCache = fetchYarnDeps {
+    yarnLock = "${src}/extensions/yarn.lock";
+    hash = "sha256-3x7qVxtW3flSUKns04ux/zAWE0q/c2sF+YeCNxogkn0=";
   };
 in
 stdenv.mkDerivation {
 
     inherit pname version src POSITRON_VERSION_MAJOR POSITRON_VERSION_MINOR POSITRON_VERSION_PATCH POSITRON_VERSION_SUFFIX;
 
+
     nativeBuildInputs = [
       yarn
+      fixup-yarn-lock
       nodejs
-      jq
+      #jq
       git
-      gcc
-      cmake
+      #gcc
+      #cmake
       pkg-config
       libX11
       libxkbfile
       libsecret
       libkrb5
+      python3
     ];
 
   buildInputs = [
    quarto
   ];
+
+  configurePhase = ''
+    runHook preConfigure
+
+    export HOME=$PWD/home
+
+    configureDependencies () {
+      yarn config --offline set yarn-offline-mirror $1
+      fixup-yarn-lock "$2/yarn.lock"
+      yarn install --offline --cwd "$2" --frozen-lockfile --ignore-platform --ignore-scripts --no-progress --non-interactive
+
+      patchShebangs "$2/node_modules/"
+    }
+
+    configureDependencies ${OfflineCache} "."
+    configureDependencies ${buildOfflineCache} "./build"
+    configureDependencies ${remoteOfflineCache} "./remote"
+    configureDependencies ${extensionsOfflineCache} "./extensions"
+
+    runHook postConfigure
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+
+    yarn --offline compile
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mv build $out
+
+    runHook postInstall
+  '';
 
   meta = {
     description = "Positron, a next-generation data science IDE";

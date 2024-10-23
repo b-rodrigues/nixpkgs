d4f50d7ecf7b25cc6f52638cc53231e6bd0dcf64
Bruno Rodrigues <bruno.rodrigues@mesr.etat.lu>
2024-10-23 15:42:53 +0200

    python3Packages.gower: init at version 0.1.2

---
+++ pkgs/development/python-modules/gower/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  buildPythonPackage,
+  setuptools,
+  fetchPypi,
+  numpy,
+  pandas,
+  scipy
+}:
+
+buildPythonPackage rec {
+
+  pname = "gower";
+  version = "0.1.2";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-NN21FY8Oi/ugk9yga5+Ie9okSZjRCvKjrYx0pu+htfY=";
+  };
+
+  patchPhase = ''
+
+    rm pyproject.toml
+
+  '';
+
+  dependencies = [ setuptools numpy scipy pandas ];
+
+  meta = with lib; {
+    description = "Gower's distance calculation in Python.";
+    homepage = "https://github.com/wwwjk366/gower";
+    license = licenses.mit;
+    maintainers = with maintainers; [ b-rodrigues ];
+  };
+}

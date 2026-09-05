9cc0437a9f0a24bf0302b18b97a4e847428413e8
b-rodrigues <brodriguesco@protonmail.com>
2026-09-05 20:59:43 +0200

    updated cline with fhs

--- pkgs/by-name/cl/cline/package.nix
+++ pkgs/by-name/cl/cline/package.nix
@@ -2,13 +2,22 @@
   lib,
   stdenvNoCC,
   fetchurl,
-  autoPatchelfHook,
-  makeBinaryWrapper,
+  buildFHSEnv,
+  bash,
   coreutils,
+  curl,
+  findutils,
+  gawk,
+  git,
+  gnugrep,
+  gnused,
+  gnutar,
+  gzip,
+  jq,
   procps,
   ripgrep,
-  versionCheckHook,
-  writableTmpDirAsHomeHook,
+  which,
+  extraPackages ? [ ],
   sources ? lib.importJSON ./sources.json,
 }:
 
@@ -17,51 +26,73 @@ let
   source =
     sources.sources.${stdenvNoCC.hostPlatform.system}
       or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
+
+  cline-unwrapped = stdenvNoCC.mkDerivation {
+    pname = "cline-unwrapped";
+    inherit version;
+
+    src = fetchurl {
+      url = "https://registry.npmjs.org/@cline/cli-${source.platform}/-/cli-${source.platform}-${version}.tgz";
+      inherit (source) hash;
+    };
+
+    # Prevent bun runtime stripping
+    dontStrip = true;
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/lib/cline
+      cp -r * $out/lib/cline/
+
+      runHook postInstall
+    '';
+
+    passthru.exePath = "/lib/cline/bin/cline";
+
+    meta = {
+      description = "Autonomous coding agent CLI - capable of creating/editing files, running commands, and more";
+      homepage = "https://cline.bot";
+      changelog = "https://github.com/cline/cline/releases";
+      license = lib.licenses.asl20;
+      maintainers = with lib.maintainers; [ b-rodrigues ];
+      platforms = lib.attrNames sources.sources;
+      mainProgram = "cline";
+      sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    };
+  };
 in
-stdenvNoCC.mkDerivation {
+buildFHSEnv {
   pname = "cline";
   inherit version;
 
-  src = fetchurl {
-    url = "https://registry.npmjs.org/@cline/cli-${source.platform}/-/cli-${source.platform}-${version}.tgz";
-    inherit (source) hash;
-  };
+  # Tools that must be available to the agent inside the FHS sandbox: the
+  # wrapped binary invokes ripgrep/procps itself, and shell commands it
+  # generates expect a standard Unix userland. Project-specific toolchains
+  # (nodejs, python, compilers, ...) can be added via `extraPackages`.
+  targetPkgs =
+    pkgs: with pkgs; [
+      bash
+      coreutils
+      curl
+      findutils
+      gawk
+      git
+      gnugrep
+      gnused
+      gnutar
+      gzip
+      jq
+      procps
+      ripgrep
+      which
+    ]
+    ++ extraPackages;
 
-  # Prevent bun runtime stripping
-  dontStrip = true;
-
-  nativeBuildInputs = [
-    makeBinaryWrapper
-  ]
-  ++ lib.optionals stdenvNoCC.hostPlatform.isElf [ autoPatchelfHook ];
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/lib/cline $out/bin
-    cp -r * $out/lib/cline/
-
-    makeWrapper $out/lib/cline/bin/cline $out/bin/cline \
-      --prefix PATH : ${
-        lib.makeBinPath [
-          coreutils
-          procps
-          ripgrep
-        ]
-      }
-
-    runHook postInstall
-  '';
-
-  nativeInstallCheckInputs = [
-    writableTmpDirAsHomeHook
-    versionCheckHook
-  ];
-  doInstallCheck = true;
-  versionCheckProgramArg = "--version";
+  runScript = "${cline-unwrapped}${cline-unwrapped.exePath}";
 
   passthru = {
-    inherit sources;
+    inherit sources cline-unwrapped;
     updateScript = ./update.sh;
   };
 
@@ -71,7 +102,7 @@ stdenvNoCC.mkDerivation {
     changelog = "https://github.com/cline/cline/releases";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ b-rodrigues ];
-    platforms = lib.attrNames sources.sources;
+    platforms = lib.platforms.linux;
     mainProgram = "cline";
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
   };
